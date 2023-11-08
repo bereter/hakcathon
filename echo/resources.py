ae6163e7949ed8502669d70d239394b9ac3ad017
Oleg <haelgi42@gmail.com>
2023-11-08 20:22:47 +0100

    init_1

---
+++ echo/resources.py
@@ -0,0 +1,14 @@
+
+Movie = 'MV'
+Concerts = 'CO'
+Exhibitions = 'EX'
+Books = 'BK'
+Cafes_Restaurants = 'CR'
+
+CATEGORIES = [
+    (Movie, 'Кино'),
+    (Concerts, 'Концерты'),
+    (Exhibitions, 'Выставки'),
+    (Books, 'Книги'),
+    (Cafes_Restaurants, 'Кафе/Рестораны'),
+]
